cbdae9f940d35265d4d81eab4110d48de6e60b72
Riverflo <78293875+riverflo88@users.noreply.github.com>
2023-11-16 01:10:48 +0000

    Update get_location.c

--- get_location.c
+++ get_location.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
-char *get_location(char *command){
+char *get_location(char *cmd1){
     char *path, *path_copy, *path_token, *file_path;
-    int command_length, directory_length;
+    int cmd_len, len_dir;
     struct stat buffer;
     
     path = getenv("PATH");
@@ -11,7 +11,7 @@ char *get_location(char *command){
         /* Duplicate the path string -> remember to free up memory for this because strdup allocates memory that needs to be freed*/ 
         path_copy = strdup(path);
         /* Get length of the command that was passed */
-        command_length = strlen(command);
+        cmd_len = strlen(cmd1);
 
 
         /* Let's break down the path variable and get all the directories available*/
@@ -19,13 +19,13 @@ char *get_location(char *command){
 
         while(path_token != NULL){
             /* Get the length of the directory*/
-            directory_length = strlen(path_token);
+            len_dir = strlen(path_token);
             /* allocate memory for storing the command name together with the directory name */
-            file_path = malloc(command_length + directory_length + 2); /* NB: we added 2 for the slash and null character we will introduce in the full command */
+            file_path = malloc(cmd_len + len_dir + 2); /* NB: we added 2 for the slash and null character we will introduce in the full command */
             /* to build the path for the command, let's copy the directory path and concatenate the command to it */
             strcpy(file_path, path_token);
             strcat(file_path, "/");
-            strcat(file_path, command);
+            strcat(file_path, cmd1);
             strcat(file_path, "\0");
 
             /* let's test if this file path actually exists and return it if it does, otherwise try the next directory */
@@ -50,9 +50,9 @@ char *get_location(char *command){
         free(path_copy);
 
         /* before we exit without luck, let's see if the command itself is a file_path that exists */
-        if (stat(command, &buffer) == 0)
+        if (stat(cmd1, &buffer) == 0)
         {
-            return (command);
+            return (cmd1);
         }
 
 
